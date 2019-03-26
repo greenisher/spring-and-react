f446d775333bc6f3acf2c01c1cd29c93e9b9d174
bgreen2 <bgreen@and.digital>
2019-03-26 13:38:47 +0000

    first commit

---
+++ src/main/java/com/example/sprandreact/SprandreactApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprandreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprandreactApplication.class, args);
+	}
+
+}
